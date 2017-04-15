72d44e2b8fd64d4ca88b615cdd501d2bce832e6b
gigantessbeta (Miyarin) <kaminomi_1101@yahoo.co.jp>
2017-04-15 19:29:44 +0900

    エラー吹いた

--- src/IngotDrop/main.php
+++ src/IngotDrop/main.php
@@ -1,7 +1,5 @@
-<?php
-
+<<?php
 namespace IngotDrop;
-
 use pocketmine\Plugin\PluginBase;
 use pocketmine\Server;
 use pocketmine\Player;
@@ -10,34 +8,27 @@ use pocketmine\event\Listener;
 use pocketmine\event\block\BlockBreakEvent;
 use pocketmine\block\Block;
 use pocketmine\item\Item;
-
-class main extends PluginBase{
-
+class main extends PluginBase implements Listener{
 	public function onEnable(){
-		$this->getServer()->getPluginManager()->registerEvents($this, new Listener(){
-			public function onBreak(BlockBreakEvent $event){
-				$player = $event -> getPlayer();
-				if($player->getGamemode() === 0){
-					$block = $event->getBlock();
-					$id = $block->getId();
-
-					switch($id){
-						case "15":
-							$item = array(Item::get(265, 0, 1));
-						break;
-
-						case "14":
-							$item = array(Item::get(266, 0, 1));
-						break;
-
-						default:
-
-						return true;
-					}
-
-					$event->setDrops($item);
-				}	
-			}
-		});
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 	}
-}
+	public function onBreak(BlockBreakEvent $event){
+		$player = $event -> getPlayer();
+		if($player->getGamemode() === 0){
+			$block = $event->getBlock();
+			$id = $block->getId();
+			switch($id){
+				case "15":
+					$item = array(Item::get(265, 0, 1));
+				break;
+				
+				case "14":
+					$item = array(Item::get(266, 0, 1));
+				break;
+				default:
+				return true;
+			}
+			
+			$event->setDrops($item);
+		}	
+	}
